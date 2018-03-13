3e0065c5f727784d67edb82a61e5fe5f0869a73a
vethastanley <vethastanley@gmail.com>
2018-03-13 15:27:21 +0530

    Cumulocity java agent

---
+++ parking-pi-agent/src/main/java/com/softwareag/parkingpi/helper/JsonHelper.java
@@ -0,0 +1,32 @@
+package com.softwareag.parkingpi.helper;
+
+import com.softwareag.parkingpi.Sensor;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class JsonHelper {
+
+    public static void copyJSONToFile(List<Sensor> sensors) {
+        JSONObject jsonObject = new JSONObject();
+        JSONArray sensorJsonArray = new JSONArray();
+        for (Sensor sensor : sensors) {
+            JSONObject simple = new JSONObject();
+            simple.put("name", sensor.getName());
+            simple.put("trig", sensor.getTrig());
+            simple.put("echo", sensor.getEcho());
+            simple.put("id", sensor.getsID());
+            sensorJsonArray.add(simple);
+        }
+
+        try(FileWriter file = new FileWriter("sendmeasure.json");) {
+            jsonObject.put("sensors", sensorJsonArray);
+            file.write(jsonObject.toJSONString());
+            file.flush();
+        } catch (IOException e) {
+        }
+    }
+}
---
+++ parking-pi-agent/src/main/java/com/softwareag/parkingpi/helper/PiProperties.java
@@ -0,0 +1,55 @@
+package com.softwareag.parkingpi.helper;
+
+import com.softwareag.parkingpi.Sensor;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+import java.util.List;
+
+/**
+ * Get name,the number of parking place we have to create,trig and echo pin for reading the measurement
+ */
+
+public class PiProperties {
+    public static PiProperties INSTANCE = new PiProperties();
+    private String piName;
+    private String piExName;
+    private List<Sensor> sensors;
+    /*  private JSONArray sensors*/
+
+    private void Initialize() {
+        JSONObject fObj = null;
+        try {
+            JSONParser parser = new JSONParser();
+            FileReader r = new FileReader("./cfg/parkingpi.json");
+            Object obj = parser.parse(r);
+            fObj = (JSONObject) obj;
+
+            //get piname and set it to the var
+            piExName = (String) fObj.get("name");
+            piName = (String) fObj.get("externalname");
+
+            JSONArray sens = (JSONArray) fObj.get("sensors");
+            for (int i = 0; i < sens.size(); i++) {
+                JSONObject sensorObj = (JSONObject) sens.get(i);
+                sensors.add(new Sensor((String) sensorObj.get("name"), (Integer) sensorObj.get("trig"), (Integer) sensorObj.get("echo")));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getPiName() {
+        return piName;
+    }
+
+    public String getPiExName() {
+        return piExName;
+    }
+
+    public List<Sensor> getSenors() {
+        return sensors;
+    }
+}
